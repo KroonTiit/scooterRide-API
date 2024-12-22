e4580a41f00d2504d6379da989bc534842e14af7
tiit.kroon <tiitkroon@gmail.com>
2024-12-22 16:26:29 +0200

    init project spring boot starter

---
+++ src/test/java/com/scooterRide/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scooterRide.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
