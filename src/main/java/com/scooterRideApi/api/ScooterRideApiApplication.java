fa3342149dbea5cb1a0a9bf6449cef78027f3a40
tiit.kroon <tiitkroon@gmail.com>
2024-12-23 12:38:24 +0200

    proper nameing

--- src/main/java/com/scooterRideApi/api/ScooterRideApiApplication.java
+++ src/main/java/com/scooterRideApi/api/ScooterRideApiApplication.java
@@ -1,13 +1,13 @@
-package com.scooterRide.demo;
+package com.scooterRideApi.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class ScooterRideApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ScooterRideApiApplication.class, args);
 	}
 
 }
--- src/test/java/com/scooterRideApi/api/DemoApplicationTests.java
+++ src/test/java/com/scooterRideApi/api/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.scooterRide.demo;
+package com.scooterRideApi.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class ScooterRideApiApplicationTests {
 
 	@Test
 	void contextLoads() {
