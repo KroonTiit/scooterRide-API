28006729e896a08411ed8b71aaf6f37428897be0
tiit.kroon <tiitkroon@gmail.com>
2024-12-28 14:46:53 +0200

    getUser data

--- src/main/java/com/scooterRideApi/api/controller/RiderController.java
+++ src/main/java/com/scooterRideApi/api/controller/RiderController.java
@@ -1,10 +1,14 @@
 package com.scooterRideApi.api.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +24,7 @@ class UserController {
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody Rider rider) {
         try {
-          riderService.registerUser(rider.getUsername(), rider.getPassword(), rider.getEmail());
+            riderService.registerUser(rider.getUsername(), rider.getPassword(), rider.getEmail());
             return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
@@ -30,9 +34,21 @@ class UserController {
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody Rider rider) {
         if (riderService.authenticateUser(rider.getUsername(), rider.getPassword())) {
-            return ResponseEntity.ok("Login successful");
+            return ResponseEntity.ok("Login successful: " + riderService.generateToken(rider.getUsername()));
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+    }
+
+    @GetMapping("/getUserData")
+    public ResponseEntity<Object> getUserData(@RequestHeader("Authorization") String token) {
+        String authToken = token.replaceFirst("Bearer ", "");
+        String username = riderService.validateToken(authToken);
+        if (username != null) {
+            Optional<Rider> user = riderService.riderRepository.findByUsername(username);
+            return ResponseEntity.ok(user.orElse(null));
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or expired token");
         }
     }
 }
