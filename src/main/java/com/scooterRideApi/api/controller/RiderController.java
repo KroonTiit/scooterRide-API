dbc63714ee330382910ba3e034abf03ebbdcd28a
tiit.kroon <tiitkroon@gmail.com>
2024-12-29 12:22:35 +0200

    Reove unnessesary function

--- src/main/java/com/scooterRideApi/api/controller/RiderController.java
+++ src/main/java/com/scooterRideApi/api/controller/RiderController.java
@@ -1,27 +1,23 @@
 package com.scooterRideApi.api.controller;
 
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.scooterRideApi.api.dto.RiderDTO;
-import com.scooterRideApi.api.model.Rider;
 import com.scooterRideApi.api.security.BearerTokenInterceptor;
 import com.scooterRideApi.api.service.RiderService;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/api/users")
 @RestController
-class UserController {
+public class RiderController {
     @Autowired
     private RiderService riderService;
 
@@ -46,17 +42,4 @@ class UserController {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
         }
     }
-
-    @GetMapping("/getUserData")
-    public ResponseEntity<Object> getUserData(@RequestHeader("Authorization") String token) {
-        String authToken = token.replaceFirst("Bearer ", "");
-        String username = bearerTokenInterceptor.validateToken(authToken);
-        if (username != null) {
-            Optional<Rider> user = riderService.riderRepository.findByUsername(username);
-            return ResponseEntity.ok(user.orElse(null));
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid or expired token");
-        }
-    }
-    //in service of time I am leaving out other CRUD functionalitys from the user
 }
